f28f42da73436713785241d110c960ef60f5d7b0
lpault29@gmail.com Ly <lpault29@gmail.com>
2018-05-09 14:16:00 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,8 @@
 def my_collect(collection)
+  new_collection = []
   i = 0 
   while i < collection.length do 
-    yield(collection[i]) 
+    new_collection << yield(collection[i]) 
     i+=1
   end 
   return collection
