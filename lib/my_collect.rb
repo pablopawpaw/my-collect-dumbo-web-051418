48f02530f95d6392287bf7a0c588fc671d506181
lpault29@gmail.com Ly <lpault29@gmail.com>
2018-05-09 14:13:07 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,32 +1,3 @@
-def my_collect(array, change)
-  students = []
-  languages = []
+def my_collect(collection)
   
-  case change
-  when "first_name"
-    i = 0 
-    while i < array.length do 
-      students.push(array[i].split(" ").first)
-      yield(array[i])
-      i+=1
-    end
-    students
-  when "language"
-    i = 0 
-    while i < array.length do 
-      languages.push(array[i].upcase)
-      yield(array[i])
-      i+=1
-    end
-    languages
-  end
-end
-
-my_collect(['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller'], "first_name") do |name|
-  name
 end 
-
-
-my_collect(['ruby', 'javascript', 'python', 'objective-c'], "language") do |lang|
-  lang
-end
