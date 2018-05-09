f0bccfcbf973280834b01c6db3578d78fe11d3f6
lpault29@gmail.com Ly <lpault29@gmail.com>
2018-05-09 14:16:13 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(collection)
+  new_collection = []
+  i = 0 
+  while i < collection.length do 
+    new_collection << yield(collection[i]) 
+    i+=1
+  end 
+  return new_collection
+end 
